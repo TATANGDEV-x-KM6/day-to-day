a1ee3298ff119fa8f0684e05dd3f15efaff52e9c
Tatang Romadhona <romadhonatatang@gmail.com>
2024-04-22 21:22:01 +0700

    feat: add upload image multi

--- chapter-6/01_media_handling/routes/index.js
+++ chapter-6/01_media_handling/routes/index.js
@@ -4,8 +4,17 @@ const { image } = require('../libs/multer');
 
 router.post('/upload/image', image.single('image'), (req, res) => {
     let imageUrl = `${req.protocol}://${req.get('host')}/images/${req.file.filename}`;
-
-    res.render('uploadedImage', { image_url: imageUrl });
+    res.json({ image_url: imageUrl });
 });
+
+router.post('/upload/images', image.array('image'), (req, res) => {
+    let imageUrls = req.files.map(file => `${req.protocol}://${req.get('host')}/images/${file.filename}`);
+    res.json({ image_urls: imageUrls });
+});
+
+// single video (mp4, mpeg, mov)
+// multiple video (mp4, mpeg, mov)
+// single file (pdf, docx, xlxx)
+// multiple file (pdf, docx, xlxx)
 
 module.exports = router;
